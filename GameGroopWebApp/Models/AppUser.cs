cc9a02707c36e8357bfcc73898c1a706e9c7d80f
gorizzont <39597252+gorizzont@users.noreply.github.com>
2022-09-20 17:06:23 +0300

    AddAppUserModel

---
+++ GameGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace GameGroopWebApp.Models
+{
+    public class AppUser
+    {
+        public int? Rank { get; set; }
+        public int? Level { get; set; }
+        public Address? Address { get; set; }
+    }
+}
