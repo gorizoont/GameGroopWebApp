1212979d7110a2e46b2c3f9115afc735aa6b4f59
gorizzont <39597252+gorizzont@users.noreply.github.com>
2022-09-20 18:02:11 +0300

    UpdateAppUserModel

--- GameGroopWebApp/Models/AppUser.cs
+++ GameGroopWebApp/Models/AppUser.cs
@@ -5,5 +5,7 @@
         public int? Rank { get; set; }
         public int? Level { get; set; }
         public Address? Address { get; set; }
+        public IEnumerable<Club> Clubs { get; set; }
+        public IEnumerable<Events> Event { get; set; }
     }
 }
