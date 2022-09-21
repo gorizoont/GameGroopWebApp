ef79b4131172a437b834db17784c5223bec63b83
gorizzont <39597252+gorizzont@users.noreply.github.com>
2022-09-21 14:06:11 +0300

    AddAppDBContext

---
+++ GameGroopWebApp/Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using GameGroopWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameGroopWebApp.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet <Events> Events { get; set; }
+        public DbSet <Club> Clubs { get; set; }
+        public DbSet <Address> Addresss { get; set; }
+    }
+}
