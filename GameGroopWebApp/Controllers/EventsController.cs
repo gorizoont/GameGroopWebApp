943b195e92a2427b2b2ebb171db5d0f06b67c50e
gorizzont <39597252+gorizzont@users.noreply.github.com>
2022-09-22 14:08:44 +0300

    Display of Events

--- GameGroopWebApp/Controllers/EventsController.cs
+++ GameGroopWebApp/Controllers/EventsController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameGroopWebApp.Data;
+using GameGroopWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameGroopWebApp.Controllers
 {
     public class EventsController : Controller
     {
+        private readonly AppDBContext _context;
+
+        public EventsController(AppDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Events> events = _context.Events.ToList();
+            return View(events);
         }
     }
 }
